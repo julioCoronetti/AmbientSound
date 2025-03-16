54219e0308b8d45eb90c0220cb709a8cddc27c2b
Julio Coronetti <timeset13@outlook.com>
2025-03-16 11:02:59 -0300

    add audio assets and update Mixer component to use correct audio paths

--- src/components/Mixer/Mixer.tsx
+++ src/components/Mixer/Mixer.tsx
@@ -6,19 +6,19 @@ export const Mixer = () => {
             <Sound
                 title="LO-FI"
                 iconName="lofi"
-                audioSrc="/src/assets/sounds/lofi.mp3"
+                audioSrc="/assets/sounds/lofi.mp3"
             />
 
             <Sound
                 title="Rain"
                 iconName="rain"
-                audioSrc="/src/assets/sounds/rain.ogg"
+                audioSrc="/assets/sounds/rain.ogg"
             />
 
             <Sound
                 title="Coffee Shop"
                 iconName="coffee"
-                audioSrc="/src/assets/sounds/coffee-shop.mp3"
+                audioSrc="/assets/sounds/coffee-shop.mp3"
             />
         </main>
     );
